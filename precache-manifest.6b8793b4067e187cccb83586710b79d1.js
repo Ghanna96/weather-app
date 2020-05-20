823ceafcdc3e97f52a44972d633770c75c5dd1be
Ghanna96 <akramghanna@outlook.com>
2020-05-20 17:13:43 +0200

    Updates

---
+++ precache-manifest.6b8793b4067e187cccb83586710b79d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6e2463e3a070d741063bf47d1dfe63f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a73e01408cb2dd3362f1",
+    "url": "/static/css/main.49654442.chunk.css"
+  },
+  {
+    "revision": "9adf672d753c880e0e9b",
+    "url": "/static/js/2.d31b1269.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d31b1269.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a73e01408cb2dd3362f1",
+    "url": "/static/js/main.e61f28e4.chunk.js"
+  },
+  {
+    "revision": "0800dffc2738f0d5cf59",
+    "url": "/static/js/runtime-main.e09fc0f4.js"
+  }
+]);
